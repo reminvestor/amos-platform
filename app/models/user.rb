8ba969365703a29cbd1c5c1acb1ac3ef23697e98
Rick Barkley <rickbarkley@Ricks-MacBook-Pro-3.local>
2025-03-28 11:03:22 -0500

    Redesign dashboard with actionable insights and upcoming features

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,7 @@ class User < ApplicationRecord
   has_many :contact_groups, dependent: :destroy
   has_many :email_templates, dependent: :destroy
   has_many :campaigns, dependent: :destroy
+  has_many :social_posts, dependent: :destroy
   
   # Methods
   def full_name
