990cc8ec1171f955bc64b58d464ebd26e088370e
Rick Barkley <rick_barkley@me.com>
2025-03-31 12:20:17 -0500

    updated to fix some errors:

--- Gemfile
+++ Gemfile
@@ -81,6 +81,9 @@ gem 'nokogiri'
 # Scheduled tasks
 gem 'clockwork'
 
+# Email validation
+gem "email_validator"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,11 +3,14 @@ class Contact < ApplicationRecord
   belongs_to :entity, optional: true
   
   # Many-to-many association with contact groups
-  has_and_belongs_to_many :contact_groups
+  has_and_belongs_to_many :contact_groups, -> { distinct }, class_name: 'ContactGroup'
   
   # Validations
-  validates :email, presence: true, email: true, uniqueness: { scope: :user_id }
-  validates :first_name, :last_name, presence: true
+  validates :email, presence: true, email: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :corporation_id, presence: true
+  validates :corporation_name, presence: true
   
   # Status options
   STATUSES = %w[active inactive unsubscribed].freeze
@@ -22,14 +25,19 @@ class Contact < ApplicationRecord
     "#{first_name} #{last_name}"
   end
   
-  # Ensure contact belongs to only one group
-  validate :single_group_membership
+  # Callbacks
+  before_save :ensure_single_group
   
   private
   
   def single_group_membership
-    if contact_groups.count > 1
-      errors.add(:base, "Contact can only belong to one group at a time")
-    end
+    return unless contact_groups.size > 1
+    errors.add(:contact_groups, "can only belong to one group at a time")
+  end
+  
+  def ensure_single_group
+    return unless contact_groups.size > 1
+    # Keep only the most recently added group
+    self.contact_groups = [contact_groups.last]
   end
 end
