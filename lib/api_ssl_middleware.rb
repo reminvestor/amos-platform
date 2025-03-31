53688477f598f93ad4f17e1a916191770bb7d363
Rick Barkley <rickbarkley@Ricks-MacBook-Pro-3.local>
2025-03-31 15:14:32 -0500

    Fix API SSL middleware loading order

--- lib/api_ssl_middleware.rb
+++ lib/api_ssl_middleware.rb
@@ -11,20 +11,16 @@ class ApiSslMiddleware
       env["HTTPS"] = "on"
       env["HTTP_X_FORWARDED_PROTO"] = "https"
       env["rack.url_scheme"] = "https"
-      
-      begin
-        @app.call(env)
-      rescue Puma::HttpParserError => e
-        # If we catch an SSL error on API, return a JSON response
-        Rails.logger.error("API SSL Error: #{e.message}")
-        [500, {"Content-Type" => "application/json"}, [{error: "API SSL Error: #{e.message}"}.to_json]]
-      rescue => e
-        # For any other error
-        Rails.logger.error("API Error: #{e.message}")
-        [500, {"Content-Type" => "application/json"}, [{error: "API Error: #{e.message}"}.to_json]]
-      end
-    else
-      @app.call(env)
     end
+    
+    @app.call(env)
+  rescue => e
+    # Log any errors
+    if defined?(Rails) && Rails.logger
+      Rails.logger.error("API Middleware Error: #{e.class.name} - #{e.message}")
+    end
+    
+    # Return a JSON error response
+    [500, {"Content-Type" => "application/json"}, ['{"error":"Internal Server Error"}']]
   end
 end 
