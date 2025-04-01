560c64ca71f4a71b19604f4b6c5840d7f07bf22d
Rick Barkley <rickbarkley@Ricks-MacBook-Pro-3.local>
2025-04-01 11:30:43 -0500

    updated

---
+++ db/migrate/20250401162753_add_default_to_solid_queue_processes_name.rb
@@ -0,0 +1,14 @@
+class AddDefaultToSolidQueueProcessesName < ActiveRecord::Migration[7.1]
+  def change
+    # First, update any existing null values
+    execute <<-SQL
+      UPDATE solid_queue_processes 
+      SET name = 'ContactProcessor-' || hostname 
+      WHERE name IS NULL;
+    SQL
+
+    # Then modify the column to have a default value
+    # Using a simple string default that will be overridden by the application
+    change_column_default :solid_queue_processes, :name, from: nil, to: 'ContactProcessor'
+  end
+end 
