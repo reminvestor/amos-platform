0f0b1fecb3bcebbe6880f0dd7412902f63188874
Rick Barkley <rickbarkley@Ricks-MacBook-Pro-3.local>
2025-09-10 14:39:40 -0500

    Fix Terraform state management in CodePipeline
    - Enable S3 backend configuration in terraform.tf
    - Update buildspec to use -reconfigure flag and refresh state
    - Migrate existing Terraform state to S3 backend
    - This ensures CodeBuild knows about existing resources

--- aws/terraform/terraform.tf
+++ aws/terraform/terraform.tf
@@ -1,7 +1,6 @@
 # Backend configuration for Terraform state
-# This will be uncommented after the S3 bucket is created
-# terraform {
-#   backend "s3" {
-#     # Backend configuration will be provided via -backend-config during init
-#   }
-# }
+terraform {
+  backend "s3" {
+    # Backend configuration will be provided via -backend-config during init
+  }
+}
