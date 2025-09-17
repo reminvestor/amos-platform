ae4c34b7e63ce5a6eaa717b3b4b3e26c71691e84
Rick Barkley <rickbarkley@Ricks-MacBook-Pro-3.local>
2025-09-17 02:02:04 -0500

    Fix tool message alignment and improve intent detection for create actions

--- app/controllers/scout_controller.rb
+++ app/controllers/scout_controller.rb
@@ -616,6 +616,9 @@ class ScoutController < ApplicationController
     session_id = session[:scout_session_id]
     return unless session_id
     
+    # Don't save empty messages
+    return if message.blank?
+    
     # Log what we're about to save
     Rails.logger.info "💾 Saving #{role} message (#{message.class}): #{message.to_s.first(200)}..."
     
