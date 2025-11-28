62efa8bd5eec9631f278b16f72ff7166d2559bd9
Rick Barkley <rick_barkley@me.com>
2025-11-28 01:00:50 -0600

    Fix typo in IrreplaceabilityAwareRouter class name

--- app/services/collaboration/irreplaceability_aware_router.rb
+++ app/services/collaboration/irreplaceability_aware_router.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Collaboration
-  class IreplaceabilityAwareRouter
+  class IrreplaceabilityAwareRouter
     def initialize(entity:)
       @entity = entity
     end
