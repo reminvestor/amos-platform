755c3f1696d6440d7edf90357b44f8664f37c63d
Rick Barkley <rickbarkley@Ricks-MacBook-Pro-3.local>
2025-11-28 00:49:40 -0600

    Fix Api::V1::BenchmarksController to inherit from Api::BaseController

--- app/controllers/api/v1/benchmarks_controller.rb
+++ app/controllers/api/v1/benchmarks_controller.rb
@@ -2,7 +2,7 @@
 
 module Api
   module V1
-    class BenchmarksController < Api::V1::BaseController
+    class BenchmarksController < Api::BaseController
       before_action :authenticate_user!
       before_action :set_entity
 
