19e45d03d0e6cab2db55e7cd358aac4b29c54db5
Rick Barkley <rick_barkley@me.com>
2025-11-28 01:14:19 -0600

    Fix migration to check if columns exist before renaming

--- db/migrate/20251127000011_rename_hash_columns_in_agent_energy_transactions.rb
+++ db/migrate/20251127000011_rename_hash_columns_in_agent_energy_transactions.rb
@@ -2,9 +2,13 @@
 
 class RenameHashColumnsInAgentEnergyTransactions < ActiveRecord::Migration[8.0]
   def change
-    # Rename 'hash' to 'ledger_hash' to avoid conflict with Ruby's built-in hash method
-    rename_column :agent_energy_transactions, :hash, :ledger_hash
-    rename_column :agent_energy_transactions, :previous_hash, :previous_ledger_hash
+    # Only rename if the old column names exist (they may already be ledger_hash if table was created fresh)
+    if column_exists?(:agent_energy_transactions, :hash)
+      rename_column :agent_energy_transactions, :hash, :ledger_hash
+    end
+    if column_exists?(:agent_energy_transactions, :previous_hash)
+      rename_column :agent_energy_transactions, :previous_hash, :previous_ledger_hash
+    end
   end
 end
 
