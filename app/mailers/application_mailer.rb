82b4f014c914df68782155bba1a4a622e9008738
Rick Barkley <rick_barkley@me.com>
2025-12-02 14:33:48 -0600

    Fix mailer root_url error by adding URL helpers to ApplicationMailer
    - Include Rails URL helpers in ApplicationMailer
    - Add default_url_options method for mailer URL generation

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,11 @@
 class ApplicationMailer < ActionMailer::Base
+  include Rails.application.routes.url_helpers
+  
   default from: ENV["MAILER_SENDER"] || "noreply@amoslabs.com"
   layout "mailer"
+  
+  # Set default URL options for mailers
+  def default_url_options
+    { host: ENV.fetch('APP_HOST', 'localhost:3000') }
+  end
 end
