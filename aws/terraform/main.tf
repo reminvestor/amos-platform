2619d246ca508bb468dfd81da8ceb323ab6f0f79
Rick Barkley <rickbarkley@Ricks-MacBook-Pro-3.local>
2025-12-02 13:33:47 -0600

    Fix ECS task definition to use variable for CPU/memory instead of hardcoded values

--- aws/terraform/main.tf
+++ aws/terraform/main.tf
@@ -435,8 +435,8 @@ resource "aws_ecs_task_definition" "app" {
   family                   = var.app_name
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"
-  memory                   = "512"
+  cpu                      = var.ecs_task_cpu
+  memory                   = var.ecs_task_memory
   execution_role_arn       = aws_iam_role.ecs_execution_role.arn
   task_role_arn            = aws_iam_role.ecs_task_role.arn
 
