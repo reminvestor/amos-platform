54ff56d968ef5fe264dab8a867571708aa6f1cbe
Rick Barkley <rick_barkley@me.com>
2025-12-04 11:25:28 -0600

    Fix SolidQueue initializer to skip during asset precompilation
    - Skip when SECRET_KEY_BASE_DUMMY is set (Docker build)
    - Skip when rake assets tasks are running
    - Wrap database connection check in rescue for build-time safety
    - Handle PG::ConnectionBad and ActiveRecord errors gracefully

--- config/initializers/solid_queue.rb
+++ config/initializers/solid_queue.rb
@@ -9,14 +9,23 @@ if defined?(SolidQueue)
 end
 
 # Configure recurring jobs
-# Run during server/worker startup, but not during rake tasks or generators
+# Run during server/worker startup, but not during rake tasks, generators, or asset precompilation
 Rails.application.config.after_initialize do
   next unless defined?(SolidQueue)
   next if $PROGRAM_NAME =~ /rake|rails\:generate|rails\:template|rails\:update|spring/
   next if Rails.env.test?
   
-  # Wait for database to be ready
-  next unless ActiveRecord::Base.connection.table_exists?('solid_queue_recurring_tasks')
+  # Skip during asset precompilation (no database available during Docker build)
+  next if ENV['SECRET_KEY_BASE_DUMMY'].present?
+  next if defined?(Rake) && Rake.application.top_level_tasks.any? { |t| t.include?('assets') }
+  
+  # Wait for database to be ready - wrap in rescue to handle build-time execution
+  begin
+    next unless ActiveRecord::Base.connection.table_exists?('solid_queue_recurring_tasks')
+  rescue ActiveRecord::NoDatabaseError, PG::ConnectionBad, ActiveRecord::ConnectionNotEstablished => e
+    Rails.logger.info "SolidQueue: Skipping recurring task setup (no database connection): #{e.class}"
+    next
+  end
   
   Rails.logger.info "SolidQueue: Configuring recurring tasks..."
   
