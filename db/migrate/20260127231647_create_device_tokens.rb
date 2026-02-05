370156f12ef943580429b9cfd7e97afe7f11adee
Rick Barkley <rick_barkley@me.com>
2026-02-05 01:07:03 -0600

    fix: update device_tokens migration to include all columns (entity_id, device info, etc.)
    The migration was out of sync with schema.rb - it was missing:
    - entity_id reference
    - device_id, device_name, device_model, os_version, app_version
    - last_used_at, deactivated_at, deactivation_reason
    - notification_preferences (jsonb)
    - Additional indexes
    
    This caused CI failures with MissingAttributeError on entity_id.

--- db/migrate/20260127231647_create_device_tokens.rb
+++ db/migrate/20260127231647_create_device_tokens.rb
@@ -4,14 +4,28 @@ class CreateDeviceTokens < ActiveRecord::Migration[8.0]
 
     create_table :device_tokens do |t|
       t.references :user, null: false, foreign_key: true
+      t.references :entity, null: false, foreign_key: true
       t.string :token, null: false
       t.string :platform, null: false  # 'ios' or 'android'
-      t.string :endpoint_arn           # SNS endpoint ARN after registration
+      t.string :platform_arn           # SNS endpoint ARN after registration
+      t.string :device_id
+      t.string :device_name
+      t.string :device_model
+      t.string :os_version
+      t.string :app_version
       t.boolean :active, default: true, null: false
+      t.datetime :last_used_at
+      t.datetime :deactivated_at
+      t.string :deactivation_reason
+      t.jsonb :notification_preferences, default: {}
 
       t.timestamps
     end
     add_index :device_tokens, :token, unique: true unless index_exists?(:device_tokens, :token)
     add_index :device_tokens, [:user_id, :active] unless index_exists?(:device_tokens, [:user_id, :active])
+    add_index :device_tokens, :active unless index_exists?(:device_tokens, :active)
+    add_index :device_tokens, :device_id unless index_exists?(:device_tokens, :device_id)
+    add_index :device_tokens, :entity_id unless index_exists?(:device_tokens, :entity_id)
+    add_index :device_tokens, :platform_arn, unique: true unless index_exists?(:device_tokens, :platform_arn)
   end
 end
