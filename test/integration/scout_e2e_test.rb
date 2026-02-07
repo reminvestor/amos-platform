13cf27ddd9fbb3f25f065230c5b1534e74659b86
Rick Barkley <rick_barkley@me.com>
2026-02-07 17:37:33 -0600

    Fix E2E: use AWS credential discovery from ~/.aws mount, not env vars

--- test/integration/scout_e2e_test.rb
+++ test/integration/scout_e2e_test.rb
@@ -30,19 +30,11 @@ class ScoutE2eTest < ActionDispatch::IntegrationTest
     end
 
     # E2E tests need REAL AWS Bedrock calls (test_helper stubs all AWS by default)
-    # Use actual credentials from Docker environment
-    real_key = ENV['AWS_ACCESS_KEY_ID']
-    real_secret = ENV['AWS_SECRET_ACCESS_KEY']
-    real_region = ENV['AWS_REGION'] || 'us-east-1'
-
-    if real_key.blank? || real_key == 'test_access_key'
-      skip "E2E tests require real AWS credentials in environment"
-    end
-
+    # The Docker container mounts ~/.aws for credential discovery via AWS_SDK_LOAD_CONFIG
     Aws.config.update(
       stub_responses: false,
-      region: real_region,
-      credentials: Aws::Credentials.new(real_key, real_secret)
+      credentials: nil,  # Clear fake creds, let SDK discover from ~/.aws or env
+      region: ENV['AWS_REGION'] || 'us-east-1'
     )
 
     sign_in @user
